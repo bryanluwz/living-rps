32238205aa8c128eb09b1016deac61b9f12c52a5
Bryan <bryanlu.my@gmail.com>
2023-07-01 16:45:08 +0800

    mostly done

--- src/main/Main.js
+++ src/main/Main.js
@@ -10,16 +10,20 @@ export default class Main extends Component {
 		this.blobs = [];
 
 		this.state = {
-			scissorsBlobCount: 20,
-			paperBlobCount: 20,
-			rockBlobCount: 20,
+			scissorsBlobCount: 10,
+			paperBlobCount: 10,
+			rockBlobCount: 10,
+			blobSize: 50,
+			isEnd: false,
 		};
 
-		this.fps = 60;
+		this.fps = 400;
 		this.canvasUpdateTimerTick = 0;
 		this.canvasUpdateTimer = setInterval(() => {
-			this.canvasUpdateTimerTick++;
-			this.canvasUpdate();
+			if (!this.state.isEnd) {
+				this.canvasUpdateTimerTick++;
+				this.canvasUpdate();
+			}
 		}, 1000 / this.fps);
 	}
 
@@ -39,9 +43,12 @@ export default class Main extends Component {
 	// Handle screen resize
 	handleResize = () => {
 		const canvas = this.canvasRef.current;
+		if (!canvas) return;
 
 		canvas.width = window.innerWidth * 0.9;
 		canvas.height = window.innerHeight * 0.8;
+
+		this.setState({ blobSize: Math.min(50, canvas.width * 0.05) });
 
 		// Rerender code here
 		this.blobs.forEach(blob => {
@@ -53,12 +60,22 @@ export default class Main extends Component {
 	// Code to update the canvas
 	canvasUpdate = () => {
 		const canvas = this.canvasRef.current;
+		if (!canvas) return;
+
 		const context = canvas.getContext('2d');
 
 		context.clearRect(0, 0, canvas.width, canvas.height);
 
 		this.renderBlobs();
 		this.updateBlobs();
+
+		// Check if game is over
+		this.blobs.forEach(blob => {
+			// If all blobs are of the same type
+			if (this.blobs.every(b => b.blobType === blob.blobType)) {
+				this.setState({ isEnd: true });
+			}
+		});
 	};
 
 	// Reset blobs
@@ -69,18 +86,20 @@ export default class Main extends Component {
 	// Init blobs
 	initBlobs = () => {
 		const canvas = this.canvasRef.current;
+		if (!canvas) return;
+
 		this.blobs = [];
 
 		for (let i = 0; i < this.state.scissorsBlobCount; i++) {
-			this.blobs.push(new Blob(canvas.width, canvas.height, "scissors", this.fps));
+			this.blobs.push(new Blob(canvas.width, canvas.height, this.state.blobSize, "scissors", this.fps));
 		}
 
 		for (let i = 0; i < this.state.paperBlobCount; i++) {
-			this.blobs.push(new Blob(canvas.width, canvas.height, "paper", this.fps));
+			this.blobs.push(new Blob(canvas.width, canvas.height, this.state.blobSize, "paper", this.fps));
 		}
 
 		for (let i = 0; i < this.state.rockBlobCount; i++) {
-			this.blobs.push(new Blob(canvas.width, canvas.height, "rock", this.fps));
+			this.blobs.push(new Blob(canvas.width, canvas.height, this.state.blobSize, "rock", this.fps));
 		}
 
 		// Each blob would also have a reference to the other blobs
@@ -93,11 +112,13 @@ export default class Main extends Component {
 	// Render blobs
 	renderBlobs = () => {
 		const canvas = this.canvasRef.current;
+		if (!canvas) return;
+
 		const context = canvas.getContext('2d');
 
 		this.blobs.forEach(blob => {
 			if (blob.img) {
-				context.drawImage(blob.img, blob.x, blob.y, 50, 50);
+				context.drawImage(blob.img, blob.x, blob.y, this.state.blobSize, this.state.blobSize);
 			}
 		});
 	};
